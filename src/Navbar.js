b4056bea44d1d749ed7068ba028efd341ab55b97
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-05 22:15:29 +0000

    style: saved stylings

--- src/Navbar.js
+++ src/Navbar.js
@@ -64,10 +64,21 @@ export default function Navbar() {
            <p> <button>Brands</button> </p>
            <p> <button>Blog</button> </p>
             </div>
-            <div>
+            <div className="navText">
                 <h2>Explore the world’s leading</h2>
                 <h2>design portfolios</h2>
+                <div>
+                    <p>Millions of designers and agencies around the world showcase their portfolio work</p>
+                 <p>on Dribbble - the home to the world’s best design and creative professionals</p>
+                </div>
+                <form>
+                    <i className="fas fa-search"></i>
+                <input type={'search'} placeholder={"search"}/>
+
+                </form>
+            
             </div>
+           
         <div className="videoWrap">
          <Video/>
         
