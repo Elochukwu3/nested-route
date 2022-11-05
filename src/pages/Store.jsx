1822b71ae960ba3ab02cf301d9f237217c7e015b
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-05 22:43:19 +0000

    feat: NavLink

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {NavLink} from "react-router-dom"
 
 export default function Store() {
   return (
-    <div>Store</div>
+    <div>
+     <p> <input type={'search'}/></p>
+      <NavLink>
+        New Product
+      </NavLink>
+      <NavLink>
+        Men Wears
+      </NavLink>
+      <NavLink>
+        Women Wears
+      </NavLink>
+    </div>
   )
 }
