12c5ab8062f111ffeaa8cee4b63519febe3bf039
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-06 14:58:54 +0000

    feat: useLocation

--- src/AuthProfile.js
+++ src/AuthProfile.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {useAuthentication} from './auth'
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 export const AuthProfile = ({children}) => {
+    const location = useLocation()
     const {user} = useAuthentication()
     if (!user) {
-        return <Navigate to={'/signin'}/>
+        return <Navigate to={'/signin'} state={{path: location.pathname}}/>
     }
   return (children)
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom'
+import {useNavigate, useLocation} from 'react-router-dom'
 import {useAuthentication} from '../auth'
 
 export const SignIn = () => {
   const {changeInput, input, logIn}= useAuthentication();
-  const navigate = useNavigate();
+  const navigate = useNavigate();//useNavigate 
+  const location = useLocation()
+  const reDirect = location.state?.path || '/'
 
   const navigatePage =()=>{
     logIn(input)
-    navigate('/', {replace: true})
+    navigate(reDirect, {replace: true})
     
   }
   return (
