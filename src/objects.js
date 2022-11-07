24757270291ee5aaafe9d3e2fe943f9a877edba4
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-07 13:37:02 +0000

    update: added images

--- src/objects.js
+++ src/objects.js
@@ -1,8 +1,8 @@
 
 const card =[
     {
-        mainImg: "./images",
-        miniImg: "",
+        mainImg: "./images/img1.jpeg",
+        miniImg: "./mini-images/mini-img1",
         hoverText:"Video Marketing",
         miniText:"Mike | Creative Mints",
         miniBtn: "pro",
