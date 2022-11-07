daaf3a1be846c0ef71ecf6b1880cf2fbf8733a80
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-07 14:37:09 +0000

    update: updated images

--- src/objects.js
+++ src/objects.js
@@ -1,43 +1,47 @@
 
-const product =[
+export const product =[
     {
         mainImg: "../images/img1.jpeg",
         miniImg: "../mini-images/mini-img1.jpeg",
         hoverText:"Video Marketing",
         miniText:"Mike | Creative Mints",
         miniBtn: "pro",
+        id:"product"
 
     },
     {
         mainImg: "../images/img2.jpeg",
-        miniImg: "../mini-images/mini-img2.jpeg",
+        miniImg: "../mini-images/mini2.jpeg",
         hoverText:"Norite Brand",
         miniText:"Open Purpose",
         miniBtn: "pro",
+        id:"product"
 
     },
     {
         mainImg: "../images/img3.jpeg",
-        miniImg: "../mini-images/mini-img3.jpeg",
+        miniImg: "../mini-images/mini3.jpeg",
         hoverText:"Hail Design",
         miniText:"Coding Gen",
         miniBtn: "pro",
-
+        id:"product"
     },
     {
         mainImg: "../images/img4.jpeg",
-        miniImg: "../mini-images/mini-img4.jpeg",
+        miniImg: "../mini-images/mini4.jpeg",
         hoverText:"D => Hike",
         miniText:"Shot Identity",
         miniBtn: "pro",
+        id:"product"
 
     },
     {
         mainImg: "../images/img5.jpeg",
-        miniImg: "../mini-images/mini-img5.jpeg",
+        miniImg: "../mini-images/mini5.jpeg",
         hoverText:"D => Hike",
         miniText:"Riyan Moni",
         miniBtn: "team",
+        id:"product"
 
     },
     {
@@ -46,6 +50,7 @@ const product =[
         hoverText:"Steay Staedy",
         miniText:"Yuri",
         miniBtn: "pro",
+        id:"product"
 
     },
     {
@@ -54,6 +59,7 @@ const product =[
         hoverText:"CyberPunk",
         miniText:"Filtran",
         miniBtn: "pro",
+        id:"product"
 
     },
     {
@@ -62,7 +68,7 @@ const product =[
         hoverText:"Norte - Brand",
         miniText:"Slab | Pixel",
         miniBtn: "team",
-
+        id:"product"
     },
     {
         mainImg: "../images/img8.jpeg",
@@ -70,17 +76,16 @@ const product =[
         hoverText:"Vixal+ kal",
         miniText:"Vixtan",
         miniBtn: "pro",
+        id:"product"
 
     },
-]
-
-const stocks =[
     {
         mainImg: "../images/img9.jpeg",
         miniImg: "../mini-images/mini-img9.jpeg",
         hoverText:"Vixal+ kal",
         miniText:"Vixtan",
         miniBtn: "pro",
-
+        id: "stocks"
     },
 ]
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React, {useState} from 'react';
-import image from '../plays/img1.jpeg';
 import mini from '../plays/mini-img1.jpeg';
 import {Video }from "../Video"
 import videoSrc from '../plays/video.mp4'
 import '../styles/Home.css'
+import {product} from '../objects.js'
  function Home() {
-  const [selectedValue, setSelectedValue] = useState('')
+  const [selectedValue, setSelectedValue] = useState('product')
+  const productArray = product.filter(e=>{
+    return e.id === selectedValue;
+  })
+  
+  console.log(productArray);
   return (
     <>
     <Video videoUrl={videoSrc}/>
@@ -13,47 +18,43 @@ import '../styles/Home.css'
     <main className='home-section-main'>
       <div className='select-div-wrapper'>
         <select id='items' className='home-selections' onChange={(e)=>setSelectedValue(e.target.value)}>
-        <option value={'item1'}>Popular</option>
+        <option value={'product'}>Popular</option>
           <option value={'item2'}>Designs</option>
-          <option value={'item3'}>Stock</option>
+          <option value={'stocks'}>Stock</option>
         </select>
       </div>
     <div className='home-images-wrapper'>
-        <div>
-          <button>case Study</button>
-        <img src={image} alt="images" className='home-img'/>
-        <div className='image-text'>
-           <div><p>Crypto City/ Web site design</p></div>
-           <div>
-          {/* <span></span> */}
-          <span className='image-text-heart'>&#10084;</span>
-        </div>
-        </div>
-        <div className='mini-view-section'>
-          <div className='mini-image-section'>
-            <span><img src={mini} alt='profile'/></span>
-            <span>Mike | Creative Mint</span>
-            <spn className='mini-btn-pro'>Pro</spn>
+      {
+        productArray.length && productArray.map(({mainImg})=>{
+          return (<div>
+            <button>case Study</button>
+          <img src={mainImg} alt="images" className='home-img'/>
+          <div className='image-text'>
+             <div><p>Crypto City/ Web site design</p></div>
+             <div>
+            {/* <span></span> */}
+            <span className='image-text-heart'>&#10084;</span>
           </div>
-          <div>
-            <p><span className='heart'>&#10084;</span>
-            <span>80</span>
-            </p>
-            <p>
-            <span className='eye'>&#128065;</span>
-            <span>5.7k</span>
-            </p>
           </div>
-        </div>
-        </div>
-        {/* <div>
-        <img src={image} alt="images" className='home-img'/>
-        </div>
-  
-        <div>
-        <img src={image} alt="images" className='home-img'/>
-        </div> */}
-  
+          <div className='mini-view-section'>
+            <div className='mini-image-section'>
+              <span><img src={mini} alt='profile'/></span>
+              <span>Mike | Creative Mint</span>
+              <span className='mini-btn-pro'>Pro</span>
+            </div>
+            <div>
+              <p><span className='heart'>&#10084;</span>
+              <span>80</span>
+              </p>
+              <p>
+              <span className='eye'>&#128065;</span>
+              <span>5.7k</span>
+              </p>
+            </div>
+          </div>
+          </div>)
+        })
+      }
     </div>
     </main>
     </section>
