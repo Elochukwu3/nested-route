f8e5b02e7161aa338e8969a42528dd483f5d2073
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-08 08:26:23 +0000

    feat: added new loader

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import {Video }from "../Video";
-import videoSrc from '../plays/video2.mp4'
+import React from "react";
+import { Video } from "../Video";
+import videoSrc from "../plays/video2.mp4";
+import aboutImg from "../plays/about.jpg";
+import '../styles/About.css'
 export default function About() {
   return (
     <>
-    <Video videoUrl={videoSrc}/>
-    <div>
-      
-    </div>
+     <div className="about-container">
+     <Video videoUrl={videoSrc} />
+      <div>
+        <section className="about-section">
+        <div className="about-inner-text">
+          <h1> The Heart of the Designer Community </h1>
+         <p>
+         We’re on a mission to build the
+           world’s best community for creatives to share, grow, and get hired.
+         </p>
+          </div>
+          <div className="about-inner">
+            <img src={aboutImg} alt="about-img"/>
+          </div>
+         
+        </section>
+      </div>
+     </div>
     </>
-  )
+  );
 }
---
+++ src/pages/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <div>
+        <div className='loader-container'>
+            <div className='loader'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
