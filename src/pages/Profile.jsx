d118a16ec39069e74b17cc73ee1a43e715142f0f
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-10 17:55:02 +0000

    feat: animation

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,15 @@
 import React, { useRef, useState } from "react";
 import "../styles/Profile.css";
+import icon1 from "../icons/app_development.svg";
+import icon2 from "../icons/data_analyst.svg";
 
 const Profile = () => {
   const [image, setImage] = useState({ fileName: "" });
-  console.log(image.fileName);
   const file = useRef();
   const chooseImage = () => {
     file.current.click();
   };
+
   return (
     <div>
       <section className="profile-container">
@@ -32,8 +34,8 @@ const Profile = () => {
                   />
                 </div>
                 <div className="profile-name">
-                  <p> Elooo</p>
-                  <p className="profile-user-constant">User</p>
+                  <h4> Elochukwu Oguaju</h4>
+                  <p className="profile-user-constant">User: Ogu1919919</p>
                 </div>
               </div>
               {/* profile location, user entrance date */}
@@ -43,7 +45,7 @@ const Profile = () => {
                     <span>
                       <i className="fas fa-map-marker-alt"></i>
                     </span>
-                    <span>From</span>
+                    <span style={{fontSize:".760em"}}>From</span>
                   </div>
                   <div className="loction-proper">
                     <span>Nigeria</span>
@@ -55,11 +57,29 @@ const Profile = () => {
                     <span>
                       <i className="fas fa-user"></i>
                     </span>
-                    <span>memeber</span>
+                    <span style={{fontSize:".760em"}}>member</span>
                   </div>
                   <div className="loction-proper">
                     <span>Jul 2020</span>
                   </div>
+                </div>
+                <div className="featured-tags">
+                    <h4>Featured Tags</h4>
+                    <div>
+                      <span>dexi</span>
+                      <span>cute</span>
+                      <span>bags</span>
+                      <span>teddy</span>
+                      <span>stock</span>
+                      <span>XE Rate</span>
+                      <span>Gilead</span>
+                      <span>menu</span>
+                      <span>gell</span>
+                      <span>web3.0</span>
+                    </div>
+                </div>
+                <div className="welcome-profile">
+                  <button>Welcome</button>
                 </div>
               </div>
             </div>
@@ -67,9 +87,30 @@ const Profile = () => {
             <div className="user-main">
               <div className="user-inner-content">
                 <div className="profile-card">
-                  <div className="card-content">hello</div>
-                  <div className="card-content">hello</div>
-                  <div className="card-content">hello</div>
+                  <div className="card-content cont-one">
+                    <img src={icon1} alt="how" />
+                    <h3>Stock</h3>
+                  </div>
+                  <div className="card-content cont-two">
+                    <img src={icon2} alt="iconss" />
+                    <h3>Trade Graph</h3>
+                  </div>
+                  <div className="card-content cont-three">
+                    <img src={icon2} alt="iconss" />
+                    <h3>Board</h3>
+                  </div>
+                </div>
+                <div className="section-wrapper">
+                      <div className="board-animation">
+                        <p style={{'--i': -1}}>AAPl</p>
+                        <p style={{'--i': -2}}>BRK-B</p>
+                        <p style={{'--i': -3}}>DIS</p>
+                        <p style={{'--i': -4}}>GE</p>
+                        <p style={{'--i': -5}}>HD</p>
+                        <p style={{'--i': -6}}>NIKE</p>
+                        <p style={{'--i': -7}}>SBUX</p>
+                        <p style={{'--i': -8}}>DOWJ</p>
+                      </div>
                 </div>
               </div>
             </div>
